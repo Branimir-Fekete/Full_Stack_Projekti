0e8dd54ebe4293837c88e5fc37795f746920ffaf
Branimir-Fekete <branimir.fekete@gmail.com>
2024-02-29 12:01:13 +0100

    Dodaci

--- MERN/MERN_Test_App/client/src/App.jsx
+++ MERN/MERN_Test_App/client/src/App.jsx
@@ -7,6 +7,11 @@ function App() {
   const [name, setName] = useState('');
   const [age, setAge] = useState(0);
   const [username, setUserName] = useState('');
+  const [updateId, setUpdateId] = useState(null);
+  const [newName, setNewName] = useState('');
+  const [newAge, setNewAge] = useState(0);
+  const [newUsername, setNewUserName] = useState('');
+  const [showUpdateFields, setShowUpdateFields] = useState(false); // Dodajemo stanje za prikaz novih polja za ažuriranje
 
   useEffect(() => {
     Axios.get('http://localhost:3001/getUsers').then((Response) => {
@@ -24,11 +29,19 @@ function App() {
     });
   }
 
-  //deleteuser
-  function deleteUser(id) {
-    Axios.delete(`http://localhost:3001/deleteUser/${id}`).then((response) => {
-      setListOfUsers(listOfUsers.filter((user) => user._id !== id));
-      alert('User deleted');
+  function updateUser(id) {
+    Axios.put(`http://localhost:3001/updateUser/${id}`, {
+      name: newName,
+      age: newAge,
+      username: newUsername,
+    }).then((Response) => {
+      console.log('User updated');
+      // Nakon što ažuriramo korisnika, postavljamo sve vrijednosti na početne
+      setNewName('');
+      setNewAge(0);
+      setNewUserName('');
+      setUpdateId(null);
+      setShowUpdateFields(false); // Sakrivamo polja za ažuriranje
     });
   }
 
@@ -37,11 +50,51 @@ function App() {
       <div className='usersDisplay'>
         {listOfUsers.map((user) => {
           return (
-            <div>
+            <div key={user._id}>
               <h1>Name: {user.name}</h1>
               <h1>Age: {user.age}</h1>
               <h1>UserName: {user.username}</h1>
-              <button onClick={() => deleteUser(user._id)}>Delete user</button>
+              {/* Omogućavamo prikazivanje polja za ažuriranje samo za odabrane korisnike */}
+              {updateId === user._id ? (
+                <div>
+                  <input
+                    type='text'
+                    placeholder='New Name...'
+                    value={newName}
+                    onChange={(event) => setNewName(event.target.value)}
+                  />
+                  <input
+                    type='number'
+                    placeholder='New Age...'
+                    value={newAge}
+                    onChange={(event) => setNewAge(event.target.value)}
+                  />
+                  <input
+                    type='text'
+                    placeholder='New UserName...'
+                    value={newUsername}
+                    onChange={(event) => setNewUserName(event.target.value)}
+                  />
+                  <button onClick={() => updateUser(updateId)}>
+                    Update user
+                  </button>
+                </div>
+              ) : (
+                <div>
+                  <button
+                    onClick={() => {
+                      // Postavljamo id korisnika koji se ažurira i prikazujemo polja za ažuriranje
+                      setUpdateId(user._id);
+                      setShowUpdateFields(true);
+                    }}
+                  >
+                    Update user
+                  </button>
+                  <button onClick={() => deleteUser(user._id)}>
+                    Delete user
+                  </button>
+                </div>
+              )}
               <p>=================</p>
             </div>
           );
@@ -51,6 +104,7 @@ function App() {
         <input
           type='text'
           placeholder='Name...'
+          value={name}
           onChange={(event) => {
             setName(event.target.value);
           }}
@@ -58,6 +112,7 @@ function App() {
         <input
           type='number'
           placeholder='Age...'
+          value={age}
           onChange={(event) => {
             setAge(event.target.value);
           }}
@@ -65,11 +120,13 @@ function App() {
         <input
           type='text'
           placeholder='UserName...'
+          value={username}
           onChange={(event) => {
             setUserName(event.target.value);
           }}
         />
-        <button onClick={createUser}>Create user</button>
+        {/* Prikazujemo dugme za kreiranje korisnika samo ako se ne prikazuju polja za ažuriranje */}
+        {!showUpdateFields && <button onClick={createUser}>Create user</button>}
       </div>
     </div>
   );
