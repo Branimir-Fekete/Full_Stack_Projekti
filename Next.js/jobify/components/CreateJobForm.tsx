fe6b95960749b337169ba8487516d6e046517301
Branimir-Fekete <branimir.fekete@gmail.com>
2024-03-07 08:17:04 +0100

    Updejt projekta

--- Next.js/jobify/components/CreateJobForm.tsx
+++ Next.js/jobify/components/CreateJobForm.tsx
@@ -1,61 +1,98 @@
 'use client';
 
-import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 
-import { Button } from '@/components/ui/button';
 import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
+  JobStatus,
+  JobMode,
+  createAndEditJobSchema,
+  CreateAndEditJobType,
+} from '@/utils/types';
 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: 'Username must be at least 2 characters.',
-  }),
-});
+import { Button } from '@/components/ui/button';
+import { Form } from '@/components/ui/form';
+
+import { CustomFormField, CustomFormSelect } from './FormComponents';
+
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { createJobAction } from '@/utils/actions';
+import { useToast } from '@/components/ui/use-toast';
+import { useRouter } from 'next/navigation';
 
 function CreateJobForm() {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<CreateAndEditJobType>({
+    resolver: zodResolver(createAndEditJobSchema),
     defaultValues: {
-      username: '',
+      position: '',
+      company: '',
+      location: '',
+      status: JobStatus.Pending,
+      mode: JobMode.FullTime,
     },
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
-  }
+  const queryClient = useQueryClient();
+  const { toast } = useToast();
+  const router = useRouter();
+  const { mutate, isPending } = useMutation({
+    mutationFn: (values: CreateAndEditJobType) => createJobAction(values),
+    onSuccess: (data) => {
+      if (!data) {
+        toast({ description: 'there was an error' });
+        return;
+      }
+      toast({ description: 'job created' });
+      queryClient.invalidateQueries({ queryKey: ['jobs'] });
+      queryClient.invalidateQueries({ queryKey: ['stats'] });
+      queryClient.invalidateQueries({ queryKey: ['charts'] });
+      router.push('/jobs');
+    },
+  });
 
+  function onSubmit(values: CreateAndEditJobType) {
+    mutate(values);
+  }
   return (
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className='space-y-8'
+        className='bg-muted p-8 rounded'
       >
-        <FormField
-          control={form.control}
-          name='username'
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input
-                  placeholder='shadcn'
-                  {...field}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type='submit'>Submit</Button>
+        <h2 className='capitalize font-semibold text-4xl mb-6'>add job</h2>
+        <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-3 items-start'>
+          <CustomFormField
+            name='position'
+            control={form.control}
+          />
+          <CustomFormField
+            name='company'
+            control={form.control}
+          />
+          <CustomFormField
+            name='location'
+            control={form.control}
+          />
+          <CustomFormSelect
+            name='status'
+            control={form.control}
+            labelText='job status'
+            items={Object.values(JobStatus)}
+          />
+          <CustomFormSelect
+            name='mode'
+            control={form.control}
+            labelText='job mode'
+            items={Object.values(JobMode)}
+          />
+          <Button
+            type='submit'
+            className='self-end capitalize'
+            disabled={isPending}
+          >
+            {isPending ? 'loading' : 'create job'}
+          </Button>
+        </div>
       </form>
     </Form>
   );
